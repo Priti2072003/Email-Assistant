18ee36638b49038bbe5c5b72c73ca4d1f364bc8a
Priti Sukhija <pritiisukhija207@gmail.com>
2026-04-27 08:26:33 +0530

    Comment out repository methods in EmailRepository
    Comment out methods for finding unreplied emails and follow-ups.

--- emailAssistant/emailAssistant/src/main/java/com/example/emailAssistant/app/repo/EmailRepository.java
+++ emailAssistant/emailAssistant/src/main/java/com/example/emailAssistant/app/repo/EmailRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EmailRepository extends JpaRepository<EmailEntity, Long> {
-    List<EmailEntity> findByRepliedFalseAndIntent(String intent);
-    List<EmailEntity> findByFollowUpSentFalseAndPriority(String priority);
+   // List<EmailEntity> findByRepliedFalseAndIntent(String intent);
+    //List<EmailEntity> findByFollowUpSentFalseAndPriority(String priority);
 }
 
